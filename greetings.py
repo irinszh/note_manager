c1366b2b28f6ee4a34d7f3b0c12a8371b423ebfb
irinszh <irinszh2016@yandex.ru>
2025-01-17 08:54:16 +0300

    Add files via upload

---
+++ greetings.py
@@ -0,0 +1,12 @@
+username = 'Ирина'
+title = 'Задание 1'
+content = 'Работать с переменными'
+status = 'Начало работы'
+created_date = '16.01.2025'
+issue_date = '31.03.2025'
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
