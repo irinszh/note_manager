1c6ebf5c75e2acb2a9dec3c1b64e9e1c433222d5
irinszh <irinszh2016@yandex.ru>
2025-01-20 08:53:01 +0300

    Add files via upload

---
+++ add_input.py
@@ -0,0 +1,13 @@
+username = input("Введите ваше имя: ")
+title = input("Введите заголовок заметки: ")
+content = input("Введите описание заметки: ")
+status = input("Введите статус заметки: ")
+created_date = input("Введите дату создания заметки формате DD.MM.YYYY:")
+issue_date = input("Введите дату истечения заметки формате DD.MM.YYYY:")
+
+print("Имя пользователя:", username)
+print("Заголовок заметки:", title)
+print("Описание заметки:", content)
+print("Статус заметки:", status)
+print("Дата создания заметки:", created_date)
+print("Дата истечения заметки:", issue_date)
